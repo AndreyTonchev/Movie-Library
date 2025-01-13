032b90d04625297c3595e67f803ef623d03d0933
AndreyTonchev <127178861+AndreyTonchev@users.noreply.github.com>
2025-01-13 14:38:03 +0200

    init

---
+++ scripts/test.js
@@ -0,0 +1,6 @@
+const API = "http://www.omdbapi.com/?apikey=d551c863&";
+
+// fetch(`http://www.omdbapi.com/?apikey=d551c863&t=Dune`)
+//     .then((res) => res.json())
+//     .then((data) => console.log(data))
+//     .catch((err) => console.error(err));
