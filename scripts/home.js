5bc03c344055bc93e193f166eb8e9d3ad9124190
AndreyTonchev <andreytonchev3@gmail.com>
2025-01-15 19:58:43 +0200

    refactor movie fetching logic and enhance UI styling

--- scripts/home.js
+++ scripts/home.js
@@ -5,33 +5,34 @@ let top250 = []; // 3 years ago
 fetch("../top.json")
     .then((res) => res.json())
     .then((data) => {
-        for (let i = 0; i <= 25; i++) {
-            console.log(data[i]);
-            fetch(`${API}i=${data[i]}`)
-                .then((res) => res.json())
-                .then((movie_data) => {
-                    console.log(movie_data);
-                    
-                    const movie = document.createElement("div");
-                    movie.classList.add("movie-wrapper");
-                    movie.innerHTML = `
-                            <img class="movie-poster" src=${movie_data.Poster} alt="img">
-                            <div class="movie-info">
-                                <h2 class="movie-title">${movie_data.Title}</h2>
-                                <h3 class="movie-genre">Adventure</h3>
-                                <div class="movie-rating">
-                                    <span class="star star1">★</span>
-                                    <span class="star star2">★</span>
-                                    <span class="star star3">★</span>
-                                    <span class="star star4">★</span>
-                                    <span class="star star5">★</span>
-                                </div>
-                            </div>
-                    `
-                    addRating(movie, movie_data);
-                    movie_list.appendChild(movie);
-                })
-                .catch((err) => console.error(err));
+        return data.slice(0, 25);
+
+        
+    })
+    .then(async (movieIds) => {
+        for (let id of movieIds) {
+            const movie_data = await fetch(`${API}i=${id}`).then(res => res.json());
+            console.log(movie_data);
+            
+            const movie = document.createElement("div");
+            movie.classList.add("movie-wrapper");
+            movie.classList.add("container");
+            movie.innerHTML = `
+                    <img class="movie-poster" src=${movie_data.Poster} alt="img">
+                    <div class="movie-info">
+                        <h2 class="movie-title">${movie_data.Title}</h2>
+                        <h3 class="movie-genre">${movie_data.Genre}</h3>
+                        <div class="movie-rating">
+                            <span class="star star1">★</span>
+                            <span class="star star2">★</span>
+                            <span class="star star3">★</span>
+                            <span class="star star4">★</span>
+                            <span class="star star5">★</span>            
+                        </div>
+                    </div>
+            `
+            addRating(movie, movie_data);
+            movie_list.appendChild(movie);
         }
     })
     .catch((err) => console.error(err));
@@ -53,5 +54,3 @@ function addRating(movie, movie_data) {
         }
     }
 }
-    
-
--- scripts/test.js
+++ scripts/test.js
@@ -1,6 +1,14 @@
 const API = "http://www.omdbapi.com/?apikey=d551c863&";
 
-// fetch(`http://www.omdbapi.com/?apikey=d551c863&t=Dune`)
-//     .then((res) => res.json())
-//     .then((data) => console.log(data))
-//     .catch((err) => console.error(err));
+fetch(`http://www.omdbapi.com/?apikey=d551c863&t=Dune`)
+    .then((res) => res.json())
+    .then((data) => {
+        let list = [];
+        for (let i = 0; i < 250; i++) {
+            console.log(data[i]);
+            
+        }
+    })
+    .catch((err) => console.error(err));
+
+
