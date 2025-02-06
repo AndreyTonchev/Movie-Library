f66a685015626e014351803bfa66c21b70ae20f4
AndreyTonchev <andreytonchev3@gmail.com>
2025-02-06 09:47:08 +0200

    Movie page changes

--- scripts/movie.js
+++ scripts/movie.js
@@ -8,8 +8,24 @@ fetch(`${API}t=${title}`)
     .then((res) => res.json())
     .then((data) => {
         console.log(data)
-        document.title = title;
+        document.title = data.Title;
+        document.getElementById("movie-poster").setAttribute("src", `${data.Poster}`);
+        document.getElementById('movie-title').textContent = data.Title;
+        document.getElementById('movie-year').textContent = `Year: ${data.Year}`;
+        document.getElementById('movie-genre').textContent = `Genre: ${data.Genre}`;
+        document.getElementById('movie-plot').textContent = `Plot: ${data.Plot}`;
+        document.getElementById('movie-director').textContent = `Director: ${data.Director}`;
+        document.getElementById('movie-actors').textContent = `Actors: ${data.Actors}`;
+        document.getElementById('movie-rating').textContent = `Rating: ${data.imdbRating}`;
     })
-    .catch((err) => console.error(err))
+    .catch((err) => console.error(err));
 
+document.getElementById('watched-btn').addEventListener('click', () => {
+    alert('Added to Watched list!');
 
+});
+
+document.getElementById('like-btn').addEventListener('click', () => {
+    alert('Added to Liked list!');
+
+});
