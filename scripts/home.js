f88bc5e755e01a369f302bc4a46951c5e85fe1c4
AndreyTonchev <andreytonchev3@gmail.com>
2025-02-06 19:05:47 +0200

    Working sorting, added login page, working navigation

--- scripts/home.js
+++ scripts/home.js
@@ -1,21 +1,53 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
+import { getAuth, signOut, onAuthStateChanged, createUserWithEmailAndPassword, updateProfile, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+
+
+const firebase_config = {
+    apiKey: "AIzaSyC43WNubF79aEGwdOR1TTE1q04dLLnbx6I",
+    authDomain: "movie-library-459ac.firebaseapp.com",
+    projectId: "movie-library-459ac",
+    storageBucket: "movie-library-459ac.firebasestorage.app",
+    messagingSenderId: "872591315249",
+    appId: "1:872591315249:web:88c076573b5bcf70166010",
+    measurementId: "G-BKKMTYLCKJ"
+};
+
+const app = initializeApp(firebase_config);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+
 const movie_list = document.querySelector(".movies-wrapper");
 const dropdown_type = document.querySelector(".dropdown-type-span");
 const search_btn = document.querySelector(".search-btn");
-
+const form = document.getElementById("add-movie-form");
 const API = 'http://www.omdbapi.com/?apikey=d551c863&';
 
-fetch("../Jsons/top.json")
-    .then((res) => res.json())
-    .then((data) => { return data.slice(0, 250); })
-    .then(async (movieIds) => {
-        for (let id of movieIds) {
-            const movie_data = await fetch(`${API}i=${id}`).then(res => res.json());
-            addMovie(movie_data);
-        }
-    }) 
-    .catch((err) => console.error(err));
+onAuthStateChanged(auth, (user) => {
+    if (user) { 
+        console.log(user);
+        fetch("Jsons/top.json")
+            .then((res) => res.json())
+            .then((data) => { return data.slice(0, 25); })
+            .then(async (movieIds) => {
+                for (let id of movieIds) {
+                    const movie_data = await fetch(`${API}i=${id}`).then(res => res.json());
+                    addMovie(movie_data);
+                }
+            }) 
+            .catch((err) => console.error(err));
 
+    } else {
+        window.location.href = "./login.html";
+        alert("Please login to view this page");
+    }
+});
 
+document.querySelector(".log-out-btn-wrapper").addEventListener("click", e=> {
+    signOut(auth)
+    .then(() => window.location="./")
+    .catch((err) => console.error(err));  
+});
 
 function addRating(movie, movie_data) {
     let rating = movie_data.Ratings[0].Value.split("/")[0];
@@ -37,9 +69,11 @@ function addMovie(movie_data) {
     const movie = document.createElement("div");
     movie.classList.add("movie-wrapper");
     movie.classList.add("container");
-    movie.setAttribute("movie-id", `${movie_data.imdbID}`)
+    movie.setAttribute("movie-id", `${movie_data.imdbID}`);
+    movie.setAttribute("movie-year", `${movie_data.Year}`);
+    movie.setAttribute("movie-rating", `${movie_data.Ratings[0].Value.split("/")[0]}`);
     movie.innerHTML = `
-            <img class="movie-poster" src=${movie_data.Poster} alt="img">
+            <img class="movie-poster" src=${movie_data.Poster} alt="No poster available">
             <div class="movie-info">
                 <h2 class="movie-title">${movie_data.Title}</h2>
                 <h3 class="movie-genre">${movie_data.Genre}</h3>
@@ -68,6 +102,8 @@ document.querySelectorAll(".dropdown-type-option").forEach(item => {
     })
 })
 
+
+// Fetches and outputs the data one at a time
 search_btn.addEventListener("click", () => {
     const search_value = document.querySelector(".search").value;
     if(!search_value) {
@@ -78,23 +114,131 @@ search_btn.addEventListener("click", () => {
     movie_list.innerHTML = "";
 
     fetch(`${API}s=${search_value}${type == "all" ? "" : "&type=" + type}`)
-    .then((res) => res.json())
-        .then((data) => data.Search.forEach(async (movie_info) => { // Map, promise.all
+        .then((res) => res.json())
+        .then((data) => {
+            if (!data.Search) {
+                alert("No movies found");
+                return;
+            }
+
+            data.Search.forEach(async (movie_info) => { // Map, promise.all
             const movie_data = await fetch(`${API}i=${movie_info.imdbID}`).then(res => res.json());
             addMovie(movie_data);
-        }))
+            });
+        })
         .catch((err) => console.error(err));
 
-})  
+});
 
-function getOffset(el) {
-    const rect = el.getBoundingClientRect();
-    return {
-      left: rect.left + window.scrollX,
-      top: rect.top + window.scrollY
+// Fetches all the data and then outputs it
+// search_btn.addEventListener("click", () => {
+//     const search_value = document.querySelector(".search").value;
+//     if (!search_value) {
+//         alert("Input movie name");
+//         return;
+//     }
+//     const type = dropdown_type.innerText.toLowerCase();
+//     movie_list.innerHTML = "";
+
+//     fetch(`${API}s=${search_value}${type === "all" ? "" : "&type=" + type}`)
+//         .then((res) => res.json())
+//         .then((data) => {
+//             if (!data.Search) {
+//                 alert("No movies found");
+//                 return;
+//             }
+
+//             const moviePromises = data.Search.map((movie_info) =>
+//                 fetch(`${API}i=${movie_info.imdbID}`)
+//                     .then((res) => res.json())
+//             );
+
+//             return Promise.all(moviePromises);
+//         })
+//         .then((movies) => {
+//             movies.forEach(addMovie);
+//         })
+//         .catch((err) => console.error(err));
+// });
+
+document.getElementById("home-btn").addEventListener("click", () => {
+    window.location.href = "./index.html";
+});
+
+document.getElementById("watched-btn").addEventListener("click", () => {
+    window.location = "./index.html?page=watched";
+});
+
+document.getElementById("favourites-btn").addEventListener("click", () => {
+    window.location.href = "./index.html?page=favourites";
+});
+
+document.getElementById("add-movie-btn").addEventListener("click", () => {
+    form.style.display = "flex";
+    document.getElementById("add-movie-cancel-btn").addEventListener("click", () => form.style.display = "none")
+});
+
+
+document.getElementById('movie-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const movieData = {
+        name: document.getElementById('movie-name').value,
+        year: document.getElementById('movie-year').value,
+        length: document.getElementById('movie-length').value,
+        rating: document.getElementById('movie-rating').value,
+        director: document.getElementById('movie-director').value,
+        plot: document.getElementById('movie-plot').value
     };
+    
+    saveMovieData(movieData);
+});
+
+function saveMovieData(movieData) {
+    console.log("Movie Data:", movieData);
+    
+    
+    fs.writeFile('Jsons/movies.json', JSON.stringify(movieData), (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log("File has been created");
+    });
 }
 
 
+document.querySelectorAll(".dropdown-sort-option").forEach(option => {
+    option.addEventListener("click", () => {
+        const sort_by = option.getAttribute("data-sort");
+        const sort_order = option.getAttribute("data-order");
+        sortMovies(sort_by, sort_order);
+    });
+});
 
+function sortMovies(criteria, order) {
+    const movies = Array.from(document.querySelectorAll(".movie-wrapper"));
+    movies.sort((a, b) => {
+        const titleA = a.querySelector(".movie-title").innerText.toUpperCase();
+        const titleB = b.querySelector(".movie-title").innerText.toUpperCase();
+        const yearA = parseInt(a.getAttribute("movie-year")) || 0;
+        const yearB = parseInt(b.getAttribute("movie-year")) || 0;
+        const ratingA = parseFloat(a.getAttribute("movie-rating")) || 0;
+        const ratingB = parseFloat(b.getAttribute("movie-rating")) || 0;
 
+        let comparison = 0;
+        if (criteria === "name") {
+            comparison = titleA.localeCompare(titleB);
+        } else if (criteria === "year") {
+            comparison = yearA - yearB;
+        } else if (criteria === "rating") {
+            comparison = ratingA - ratingB;
+        }
+
+        return order === "asc" ? comparison : -comparison;
+    });
+
+    const movie_list = document.querySelector(".movies-wrapper");
+    movie_list.innerHTML = "";
+    movies.forEach(movie => movie_list.appendChild(movie));
+}
---
+++ scripts/login.js
@@ -0,0 +1,137 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
+
+
+const firebase_config = {
+    apiKey: "AIzaSyC43WNubF79aEGwdOR1TTE1q04dLLnbx6I",
+    authDomain: "movie-library-459ac.firebaseapp.com",
+    projectId: "movie-library-459ac",
+    storageBucket: "movie-library-459ac.firebasestorage.app",
+    messagingSenderId: "872591315249",
+    appId: "1:872591315249:web:88c076573b5bcf70166010",
+    measurementId: "G-BKKMTYLCKJ"
+};
+
+const app = initializeApp(firebase_config);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+
+const user_name_field = document.getElementById("username-field");
+const password_field = document.getElementById("password-field");
+
+const sign_up_btn = document.getElementById("sign-up-btn");
+const log_in_btn = document.getElementById("log-in-btn");
+
+const sign_up_link = document.querySelector(".sign-up-link");
+const log_in_link = document.querySelector(".log-in-link");
+
+const info_span = document.querySelector(".info-span");
+
+const showErrorMsg = (message) => {
+    document.getElementById("validation-container").style.display = "flex";
+    document.getElementById("validation-container").textContent = message;
+}
+
+sign_up_link.addEventListener("click", (e) => {
+    e.preventDefault();
+    log_in_btn.style.display = "none";
+    sign_up_btn.style.display = "flex";
+    user_name_field.style.display = "inline-block";
+
+    log_in_link.style.display = "flex";
+    sign_up_link.style.display = "none";
+    info_span.textContent = "You have an accout?";
+});
+
+log_in_link.addEventListener("click", (e) => {
+    e.preventDefault();
+    log_in_btn.style.display = "flex";
+    sign_up_btn.style.display = "none";
+    user_name_field.style.display = "none";
+
+    log_in_link.style.display = "none";
+    sign_up_link.style.display = "flex";
+    info_span.textContent = "You don't have an accout?";
+});
+
+sign_up_btn.addEventListener("click", async (e) => {
+    e.preventDefault();
+    
+    const fields = document.querySelectorAll(".authhenticator-field");
+
+    const email = fields[0].value;
+    const username = fields[1].value;
+    const password = fields[2].value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        const user = userCredential.user;
+
+        updateProfile(auth.currentUser, {
+            displayName: username,
+            photoURL: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fad%2F0d%2F24%2Fad0d24cc5fd5909e1a68e7d9c38f34aa.png&f=1&nofb=1&ipt=1da3f6c787a3f428b93abf2b84232a610a8ac11f2f50741f7d39dc5e8a3f2c67&ipo=images",
+          }).then(() => {
+            window.location="./index.html"
+          }).catch((error) => {
+            console.log("Error:" + error);
+          });
+          
+    })
+    .catch((error) => {
+        const error_code = error.code;
+
+        switch (error_code) {
+            case "auth/email-already-in-use": {
+                showErrorMsg("The email is already in use");
+                break;
+            }
+            case "auth/invalid-email": {
+                showErrorMsg("The email is invalid");
+                break;
+            }
+            case "auth/missing-email": {
+                showErrorMsg("Missing email");
+                break;
+            }
+            case "auth/missing-password": {
+                showErrorMsg("Missing password");
+                break;
+            }
+        }
+    });
+});
+
+log_in_btn.addEventListener("click", e => {
+    e.preventDefault();
+
+    const fields = document.querySelectorAll(".authhenticator-field");
+
+    const email = fields[0].value;
+    const password = fields[2].value;
+
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        const user = userCredential.user;
+        window.location="./index.html";
+    })
+    .catch((error) => {
+        const error_code = error.code;
+        
+
+        switch (error_code) {
+            case "auth/email-already-in-use": {
+                showErrorMsg("The email is already in use");
+                break;
+            }
+            case "auth/invalid-credential": {
+                showErrorMsg("The email or the password is incorrect");
+                break;
+            }
+            case "auth/missing-password": {
+                showErrorMsg("Missing password");
+                break;
+            }
+        }
+    });
+});
